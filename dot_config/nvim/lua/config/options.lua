c19cf4cb88220e03117b2680a1c23523dd0005b6
Philip Korsager Nickel <philipnickel@outlook.dk>
2025-12-07 08:59:41 +0100

    linewrap

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -13,3 +13,10 @@ vim.opt.number = true
 vim.opt.relativenumber = false
 vim.g.autoformat = false
 vim.g.maplocalleader = "-"
+
+-- Soft wrap (visual only, doesn't modify the file)
+vim.opt.wrap = true
+vim.opt.linebreak = true      -- Wrap at word boundaries, not mid-word
+vim.opt.breakindent = true    -- Preserve indentation on wrapped lines
+vim.opt.breakindentopt = "shift:2"  -- Extra indent for wrapped lines
+vim.opt.showbreak = "↪ "      -- Visual indicator for wrapped lines
